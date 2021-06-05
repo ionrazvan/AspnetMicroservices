f647c7740ff4a60ae6938c6aa9c3f58012ad99d2
Razvan Ion <ionrazvan@hotmail.com>
2021-06-05 20:10:50 +0300

    Add Discount controller and register the repository service.

---
+++ src/Services/Discount/Discount.API/Controllers/DiscountController.cs
@@ -0,0 +1,53 @@
+﻿using Discount.API.Entities;
+using Discount.API.Repositories;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace Discount.API.Controllers
+{
+    [ApiController]
+    [Route("api/v1/[controller]")]
+    public class DiscountController: ControllerBase
+    {
+        private IDiscountRepository repository;
+
+        public DiscountController(IDiscountRepository repository)
+        {
+            this.repository = repository;
+        }
+
+        [HttpGet("{productName}", Name = nameof(GetDiscount))]
+        [ProducesResponseType(typeof(Coupon), (int)HttpStatusCode.OK)]
+        public async Task<ActionResult<Coupon>> GetDiscount(string productName)
+        {
+            var coupon = await repository.GetDiscount(productName);
+            return Ok(coupon);
+        }
+
+        [HttpPost]
+        [ProducesResponseType(typeof(Coupon), (int)HttpStatusCode.Created)]
+        public async Task<ActionResult<Coupon>> CreateDiscount([FromBody] Coupon coupon)
+        {
+            await repository.CreateDiscount(coupon);
+            return CreatedAtRoute(nameof(GetDiscount), new { productName = coupon.ProductName }, coupon);
+        }
+
+        [HttpPut]
+        [ProducesResponseType(typeof(bool), (int)HttpStatusCode.OK)]
+        public async Task<ActionResult<bool>> UpdateDiscount([FromBody] Coupon coupon)
+        {
+            return Ok(await repository.UpdateDiscount(coupon));
+        }
+
+        [HttpDelete("{productName}")]
+        [ProducesResponseType(typeof(bool), (int)HttpStatusCode.OK)]
+        public async Task<ActionResult<bool>> DeleteDiscount(string productName)
+        {
+            return Ok(await repository.DeleteDiscount(productName));
+        }
+    }
+}
