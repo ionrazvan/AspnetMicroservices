50d51b3517641f3a8d17344e9c6efe0c6cc42348
Razvan Ion <ionrazvan@hotmail.com>
2021-06-09 19:18:43 +0300

    Add Discount Grpc client in the Basket.API

--- src/Services/Basket/Basket.API/Controllers/BasketController.cs
+++ src/Services/Basket/Basket.API/Controllers/BasketController.cs
@@ -29,6 +29,7 @@ namespace Basket.API.Controllers
         [ProducesResponseType(typeof(ShoppingCart), (int)HttpStatusCode.OK)]
         public async Task<ActionResult<ShoppingCart>> UpdateBasket([FromBody] ShoppingCart basket)
         {
+            // TODO: Communicate with Discount.Grpc and Calculate latest prices of product into the shopping cart
             return Ok(await repository.UpdateBasket(basket));
         }
 
